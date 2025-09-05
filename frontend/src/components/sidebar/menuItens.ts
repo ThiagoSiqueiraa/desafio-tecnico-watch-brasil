7e3912a57465646ebf5d8b114dd41747913fe936
Thiago Siqueira <thiagosiqueira@unifei.edu.br>
2025-09-05 19:32:33 -0300

    feat: adiciona no menu a opção time

--- frontend/src/components/sidebar/menuItens.ts
+++ frontend/src/components/sidebar/menuItens.ts
@@ -1,20 +1,22 @@
-
 export default [
-	{
-		title: 'Dashboard',
-		icon: 'mdi-star',
-		route: '/',
-	},
-    {
-        title: 'Gerenciar tarefas',
-        icon: 'mdi-format-list-bulleted',
-        route: '/manage-tasks',
-    },
-    {
-        title: 'Criar tarefa',
-        icon: 'mdi-plus-box',
-        route: '/create-task',
-    }
-
-
+  {
+    title: 'Dashboard',
+    icon: 'mdi-star',
+    route: '/',
+  },
+  {
+    title: 'Gerenciar tarefas',
+    icon: 'mdi-format-list-bulleted',
+    route: '/manage-tasks',
+  },
+  {
+    title: 'Criar tarefa',
+    icon: 'mdi-plus-box',
+    route: '/create-task',
+  },
+  {
+    title: 'Time',
+    icon: 'mdi-account-group',
+    route: '/team',
+  },
 ]
