e7ed598a438ec025fc441fdadfb5429ceed57ed6
Thiago Siqueira <thiagosiqueira@unifei.edu.br>
2025-09-04 22:20:45 -0300

    feat: cria esboço da sidebar

---
+++ frontend/src/views/dashboard/menuItens.ts
@@ -0,0 +1,20 @@
+
+export default [
+	{
+		title: 'Dashboard',
+		icon: 'mdi-star',
+		route: '/',
+	},
+    {
+        title: 'Gerenciar tarefas',
+        icon: 'mdi-format-list-bulleted',
+        route: '/settings',
+    },
+    {
+        title: 'Criar tarefa',
+        icon: 'mdi-plus-box',
+        route: '/create-task',
+    }
+
+
+]
