477b14d03f4e787d4601366e65a019a749170bd0
Thiago Siqueira <thiagosiqueira@unifei.edu.br>
2025-09-04 21:36:13 -0300

    feat: protege as rotas (não há serviço de autenticação implementado ainda) & cria esqueleto do dashboard

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,24 +1,38 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import LoginView from '../views/login/Index.vue'
 import RegisterView from '../views/register/Index.vue'
+import DashboardView from '../views/dashboard/Index.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
+      name: 'dashboard',
+      component: DashboardView,
+      meta: { requiresAuth: true },
+    },
+    {
+      path: '/login',
       name: 'login',
       component: LoginView,
     },
     {
       path: '/register',
       name: 'register',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: RegisterView
+      component: RegisterView,
     },
   ],
+})
+
+router.beforeEach((to, from, next) => {
+    const isAuthenticated = true;
+
+    if (to.meta.requiresAuth && !isAuthenticated) {
+        next('/login'); // Redireciona para a página de login se não estiver autenticado
+    } else {
+        next(); // Permite a navegação
+    }
 })
 
 export default router
