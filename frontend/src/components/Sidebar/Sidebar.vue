37ff0ab5b675528992200b70b9994f8566ced2bc
Thiago Siqueira <thiagosiqueira@unifei.edu.br>
2025-09-04 22:24:58 -0300

    refactor: separa sidebar em um componente

---
+++ frontend/src/components/Sidebar/Sidebar.vue
@@ -0,0 +1,37 @@
+<script lang="ts" setup>
+import menuItens from './menuItens'
+const menu = menuItens
+</script>
+
+<template>
+  <v-card>
+    <v-layout>
+      <v-navigation-drawer permanent>
+        <v-list>
+          <v-list-item
+            prepend-avatar="https://randomuser.me/api/portraits/women/85.jpg"
+            subtitle="<e-mail do usuário>"
+            title="<nome do usuário>"
+          ></v-list-item>
+        </v-list>
+
+        <v-divider></v-divider>
+
+        <v-list nav>
+          <v-list-item
+            v-for="(item, index) in menu"
+            :key="index"
+            :prepend-icon="item.icon"
+            :title="item.title"
+            :to="item.route"
+          ></v-list-item>
+        </v-list>
+
+      </v-navigation-drawer>
+
+      <v-main style="height: 100vh">
+        <div>oii</div>
+      </v-main>
+    </v-layout>
+  </v-card>
+</template>
--- frontend/src/views/dashboard/Index.vue
+++ frontend/src/views/dashboard/Index.vue
@@ -1,37 +1,7 @@
 <script lang="ts" setup>
-import menuItens from './menuItens'
-const menu = menuItens
+import Sidebar from '../../components/Sidebar/Sidebar.vue'
 </script>
 
 <template>
-  <v-card>
-    <v-layout>
-      <v-navigation-drawer permanent>
-        <v-list>
-          <v-list-item
-            prepend-avatar="https://randomuser.me/api/portraits/women/85.jpg"
-            subtitle="<e-mail do usuário>"
-            title="<nome do usuário>"
-          ></v-list-item>
-        </v-list>
-
-        <v-divider></v-divider>
-
-        <v-list nav>
-          <v-list-item
-            v-for="(item, index) in menu"
-            :key="index"
-            :prepend-icon="item.icon"
-            :title="item.title"
-            :to="item.route"
-          ></v-list-item>
-        </v-list>
-
-      </v-navigation-drawer>
-
-      <v-main style="height: 100vh">
-        <div>oii</div>
-      </v-main>
-    </v-layout>
-  </v-card>
+  <Sidebar />
 </template>
