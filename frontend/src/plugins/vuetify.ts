60ba6b58d1764df6ca59463df0e99093b2dc2e63
Thiago Siqueira <thiagosiqueira@unifei.edu.br>
2025-09-04 21:04:31 -0300

    refactor: move a configuração do vuetify para plugins/vuetify.ts

---
+++ frontend/src/plugins/vuetify.ts
@@ -0,0 +1,32 @@
+// plugins/vuetify.ts
+import 'vuetify/styles'
+
+// Vuetify
+import 'vuetify/styles'
+import '@mdi/font/css/materialdesignicons.css'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+export default createVuetify({
+  components,
+  directives,
+  theme: {
+    defaultTheme: 'myCustomTheme',
+    themes: {
+      myCustomTheme: {
+        dark: false, // ou true se quiser dark mode
+        colors: {
+          primary: '#1867C0', // azul padrão do Vuetify
+          secondary: '#5CBBF6',
+          error: '#FF5252',
+          success: '#4CAF50',
+          warning: '#FB8C00',
+        },
+      },
+    },
+  },
+  icons: {
+    defaultSet: 'mdi',
+  },
+})
