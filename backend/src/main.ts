b2de6861e9c4e8f83177dfbb653f0348d0bb2482
Thiago Siqueira <thiagosiqueira@unifei.edu.br>
2025-09-06 20:12:19 -0300

    feat: persiste projeto no banco

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,6 +1,9 @@
 import express, { Request, Response } from "express";
+import pgPromise from "pg-promise";
 const app = express();
 app.use(express.json());
+
+const connection = pgPromise()("postgres://postgres:123456@db:5432/app");
 
 interface CreateProjectBody {
   name: string;
@@ -9,8 +12,12 @@ app.post(
   "/projects",
   async (req: Request<{}, {}, CreateProjectBody>, res: Response) => {
     const { name } = req.body;
+    const id = await connection.query(
+      "INSERT INTO app.projects (name, owner_user_id) VALUES ($1, $2) RETURNING id",
+      [name, 1]
+    );
 
-    res.json({ id: Math.floor(Math.random() * 1000), name });
+    res.json({ id: id[0].id, name });
   }
 );
 
