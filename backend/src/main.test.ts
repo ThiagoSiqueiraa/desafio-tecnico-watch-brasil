4bb3437a04d0421b04c0c862dfdc99aaba1ae5a9
Thiago Siqueira <thiagosiqueira@unifei.edu.br>
2025-09-06 20:27:00 -0300

    test: cria teste para verificar se projeto foi deletado com sucesso

--- backend/src/main.test.ts
+++ backend/src/main.test.ts
@@ -39,3 +39,32 @@ test("Deve retornar todos os projetos do usuário", async () => {
         expect(project).toHaveProperty('name');
     });
 });
+
+test("Deve deletar um projeto com sucesso", async () => {
+
+    //Given
+    const input = {
+        name: `Projeto-${Math.random().toString(36).substring(2, 10)}`,
+    };
+    const responseCreateProject = await axios.post(
+        "http://localhost:3000/projects",
+        input
+    );
+    const outputCreateProject = responseCreateProject.data;
+    expect(responseCreateProject.status).toBe(200);
+    expect(outputCreateProject.id).toBeDefined();
+    const projectId = outputCreateProject.id;
+
+    //When
+    const responseDelete = await axios.delete(`http://localhost:3000/projects/${projectId}`);
+    //Then
+    expect(responseDelete.status).toBe(204);
+    
+    //Verifica se o projeto foi realmente deletado
+    try {
+        await axios.get(`http://localhost:3000/project/${projectId}`);
+    } catch (error: any) {
+        expect(error.response.status).toBe(404);
+        expect(error.response.data.message).toBe("Projeto não encontrado");
+    }
+});
