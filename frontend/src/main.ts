5a7d287af17234fafb2b4564ae4916bf43e747ce
Thiago Siqueira <thiagosiqueira@unifei.edu.br>
2025-09-06 20:33:28 -0300

    chore: prove injeção do projectgateway

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -4,10 +4,10 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import vuetify from './plugins/vuetify'
-
+import ProjectGateway from './gateway/ProjectGateway'
 
 const app = createApp(App)
-
 app.use(vuetify)
 app.use(router)
+app.provide('projectGateway', new ProjectGateway())
 app.mount('#app')
