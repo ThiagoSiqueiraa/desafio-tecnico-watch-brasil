49eebe9031133b56792e6ba83fd59a2552bd4f45
Thiago Siqueira <thiagosiqueira@unifei.edu.br>
2025-09-07 11:21:27 -0300

    feat: cria rota de login de usuario

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,7 +1,7 @@
 import express, { Request, Response } from "express";
 import pgPromise from "pg-promise";
 import cors from "cors";
-import { hash } from "bcrypt";
+import { compare, hash } from "bcrypt";
 const app = express();
 app.use(express.json());
 app.use(cors());
@@ -139,6 +139,34 @@ app.post("/users", async (req: Request, res: Response) => {
   );
 
   res.json({ id: id[0].id, name, email });
+});
+
+app.post("/login", async (req: Request, res: Response) => {
+  const { email, password } = req.body;
+  if (!email) {
+    return res.status(400).json({ message: "Email é obrigatório" });
+  }
+
+  if (!password) {
+    return res.status(400).json({ message: "Senha é obrigatória" });
+  }
+  
+  const user = await connection.query(
+    "SELECT id, name, email, password FROM app.users WHERE email = $1",
+    [email]
+  );
+  if (user.length === 0) {
+    return res.status(404).json({ message: "E-mail ou senha inválido" });
+  }
+  
+  const userData = user[0];
+  const isPasswordValid = await compare(password, userData.password);
+  if (!isPasswordValid) {
+    return res.status(401).json({ message: "E-mail ou senha inválido" }); 
+    //Retorna essa mensagem para não deixar claro se o e-mail ou a senha estão incorretos
+  }
+
+  res.json({ id: userData.id, name: userData.name, email: userData.email });
 });
 
 
