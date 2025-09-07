8a1935f5532cdf3c3caf223a0c9c18d82c1172a6
Thiago Siqueira <thiagosiqueira@unifei.edu.br>
2025-09-07 10:43:54 -0300

    feat: adiciona rota de criação de tarefa

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -14,10 +14,10 @@ app.post(
   "/projects",
   async (req: Request<{}, {}, CreateProjectBody>, res: Response) => {
     const { name } = req.body;
-    if(!name){
-        return res.status(400).json({ message: "Nome do projeto é obrigatório" });
+    if (!name) {
+      return res.status(400).json({ message: "Nome do projeto é obrigatório" });
     }
-    
+
     const id = await connection.query(
       "INSERT INTO app.projects (name, owner_user_id) VALUES ($1, $2) RETURNING id",
       [name, 1]
@@ -54,6 +54,67 @@ app.delete("/projects/:id", async (req: Request, res: Response) => {
   const { id } = req.params;
   await connection.query("DELETE FROM app.projects WHERE id = $1", [id]);
   res.status(204).send();
+});
+
+app.post("/tasks", async (req: Request, res: Response) => {
+  const { title, projectId, status, priority, dueDate } = req.body;
+  if (!title) {
+    return res.status(400).json({ message: "Título é obri" });
+  }
+
+  if (!projectId) {
+    return res.status(400).json({ message: "ID do projeto é obrigatório" });
+  }
+
+  if (!status) {
+    return res.status(400).json({ message: "Status é obrigatório" });
+  }
+
+  if (!["pending", "in_progress", "completed"].includes(status)) {
+    return res
+      .status(400)
+      .json({
+        message:
+          "Status inválido, informe os status possíveis: 'pending', 'in_progress', 'completed'",
+      });
+  }
+
+  if (!priority) {
+    return res.status(400).json({ message: "Prioridade é obrigatória" });
+  }
+
+  const priorityDictonary: {
+    [key: string]: number;
+  } = { low: 1, medium: 2, high: 3 };
+
+  if (!dueDate) {
+    return res
+      .status(400)
+      .json({ message: "Data de vencimento é obrigatória" });
+  }
+
+  const project = await connection.query(
+    "SELECT id FROM app.projects WHERE id = $1",
+    [projectId]
+  );
+
+  if (project.length === 0) {
+    return res.status(404).json({ message: "Projeto não encontrado" });
+  }
+
+  const id = await connection.query(
+    "INSERT INTO app.tasks (title, project_id, status, priority, due_date) VALUES ($1, $2, $3, $4, $5) RETURNING id",
+    [title, projectId, status, priorityDictonary[priority], dueDate]
+  );
+
+  res.json({
+    id: id[0].id,
+    title,
+    projectId,
+    status,
+    priority: priority,
+    dueDate,
+  });
 });
 
 console.log("Server running on http://localhost:3000");
