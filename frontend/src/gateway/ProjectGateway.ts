235f686a73a5d9abe86e476c2dfbbf43ffffff6b
Thiago Siqueira <thiagosiqueira@unifei.edu.br>
2025-09-07 14:40:45 -0300

    refactor: lint

--- frontend/src/gateway/ProjectGateway.ts
+++ frontend/src/gateway/ProjectGateway.ts
@@ -1,38 +1,34 @@
-import axios from 'axios';
+import axios from 'axios'
 
 export interface Project {
-    id: number;
-    name: string;
-    // Adicione outros campos conforme necessário
+  id: number
+  name: string
 }
 
 export default class ProjectGateway {
-    private baseUrl: string = 'http://localhost:3000'; 
+  private baseUrl: string = 'http://localhost:3000'
 
+  async list(): Promise<Project[]> {
+    const response = await axios.get<Project[]>(`${this.baseUrl}/projects?userId=1`)
+    return response.data
+  }
 
+  async getById(id: number): Promise<Project> {
+    const response = await axios.get<Project>(`${this.baseUrl}/projects/${id}`)
+    return response.data
+  }
 
-    async list(): Promise<Project[]> {
-        const response = await axios.get<Project[]>(`${this.baseUrl}/projects`);
-        return response.data;
-    }
+  async create(title: string): Promise<Project> {
+    const response = await axios.post<Project>(`${this.baseUrl}/projects`, { name: title })
+    return response.data
+  }
 
-    async getById(id: number): Promise<Project> {
-        const response = await axios.get<Project>(`${this.baseUrl}/projects/${id}`);
-        return response.data;
-    }
+  async update(id: number, project: Partial<Project>): Promise<Project> {
+    const response = await axios.put<Project>(`${this.baseUrl}/projects/${id}`, project)
+    return response.data
+  }
 
-    async create(title: string): Promise<Project> {
-        console.log("oiki")
-        const response = await axios.post<Project>(`${this.baseUrl}/projects`, { name: title });
-        return response.data;
-    }
-
-    async update(id: number, project: Partial<Project>): Promise<Project> {
-        const response = await axios.put<Project>(`${this.baseUrl}/projects/${id}`, project);
-        return response.data;
-    }
-
-    async delete(id: number): Promise<void> {
-        await axios.delete(`${this.baseUrl}/projects/${id}`);
-    }
+  async delete(id: number): Promise<void> {
+    await axios.delete(`${this.baseUrl}/projects/${id}`)
+  }
 }
