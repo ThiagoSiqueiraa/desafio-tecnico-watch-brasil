47a782a706f757c4c45f1b5d8d63930d104ba9d8
Thiago Siqueira <thiagosiqueira@unifei.edu.br>
2025-09-07 14:25:07 -0300

    fix: ajusta store

---
+++ frontend/src/stores/auth.ts
@@ -0,0 +1,67 @@
+// src/stores/auth.ts
+import { defineStore } from 'pinia'
+import { ref, computed, inject, watch } from 'vue'
+import type AuthGateway from '@/gateway/AuthGateway'
+import type { UserAuthenticated } from '@/gateway/AuthGateway'
+
+type User = { id: number; name: string; email: string; role?: string }
+
+const TOKEN_KEY = 'app:token'
+const USER_KEY = 'app:user'
+
+export const useAuthStore = defineStore('auth', () => {
+  const token = ref<string>(localStorage.getItem(TOKEN_KEY) || '')
+  const user = ref<User | null>(
+    localStorage.getItem(USER_KEY) ? JSON.parse(localStorage.getItem(USER_KEY)!) : null,
+  )
+  const loading = ref(false)
+  const error = ref('')
+
+  const isAuthenticated = computed(() => !!token.value)
+  const role = computed(() => user.value?.role || 'user')
+
+  async function login(email: string, password: string) {
+    console.log('inside auth')
+    loading.value = true
+    error.value = ''
+    try {
+      // espere algo como { token, user }
+      const authGateway = inject<AuthGateway>('authGateway')
+      if (authGateway) throw new Error('authGateway não provido (provide)')
+      const data = (await authGateway!.login({ email, password })) as UserAuthenticated
+      token.value = data.token
+      user.value = {
+        email: data.email,
+        id: data.id,
+        name: data.name,
+      }
+    } catch (err: any) {
+      error.value = err?.response?.data?.message || 'Falha no login'
+      throw err
+    } finally {
+      loading.value = false
+    }
+  }
+
+  function logout() {
+    token.value = ''
+    user.value = null
+  }
+
+  async function refresh() {
+    // se seu backend expõe /auth/refresh
+    // const data = await authGateway.refresh()
+    // token.value = data.token
+  }
+
+  // persiste automaticamente
+  watch(token, (t) => (t ? localStorage.setItem(TOKEN_KEY, t) : localStorage.removeItem(TOKEN_KEY)))
+  watch(
+    user,
+    (u) =>
+      u ? localStorage.setItem(USER_KEY, JSON.stringify(u)) : localStorage.removeItem(USER_KEY),
+    { deep: true },
+  )
+
+  return { token, user, loading, error, isAuthenticated, role, login, logout, refresh }
+})
