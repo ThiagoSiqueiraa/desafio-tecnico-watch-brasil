c115734e0822d1276551f4e8a55da628892f2658
Thiago Siqueira <thiagosiqueira@unifei.edu.br>
2025-09-06 21:04:25 -0300

    teste: teste para verificar validação da regra de neogcio, nome do projeto obrigatorio

--- backend/src/main.test.ts
+++ backend/src/main.test.ts
@@ -24,47 +24,64 @@ test("Deve criar um projeto com sucesso", async () => {
   expect(outputGetProject.name).toBe(input.name);
 });
 
-
 test("Deve retornar todos os projetos do usuário", async () => {
-    //Given
-    const userId = 1;
-    //When
-    const response = await axios.get(`http://localhost:3000/projects?userId=${userId}`);
-    const projects = response.data;
-    //Then
-    expect(response.status).toBe(200);
-    expect(Array.isArray(projects)).toBe(true);
-    projects.forEach((project: any) => {
-        expect(project).toHaveProperty('id');
-        expect(project).toHaveProperty('name');
-    });
+  //Given
+  const userId = 1;
+  //When
+  const response = await axios.get(
+    `http://localhost:3000/projects?userId=${userId}`
+  );
+  const projects = response.data;
+  //Then
+  expect(response.status).toBe(200);
+  expect(Array.isArray(projects)).toBe(true);
+  projects.forEach((project: any) => {
+    expect(project).toHaveProperty("id");
+    expect(project).toHaveProperty("name");
+  });
 });
 
 test("Deve deletar um projeto com sucesso", async () => {
+  //Given
+  const input = {
+    name: `Projeto-${Math.random().toString(36).substring(2, 10)}`,
+  };
+  const responseCreateProject = await axios.post(
+    "http://localhost:3000/projects",
+    input
+  );
+  const outputCreateProject = responseCreateProject.data;
+  expect(responseCreateProject.status).toBe(200);
+  expect(outputCreateProject.id).toBeDefined();
+  const projectId = outputCreateProject.id;
 
-    //Given
-    const input = {
-        name: `Projeto-${Math.random().toString(36).substring(2, 10)}`,
-    };
-    const responseCreateProject = await axios.post(
-        "http://localhost:3000/projects",
-        input
-    );
-    const outputCreateProject = responseCreateProject.data;
-    expect(responseCreateProject.status).toBe(200);
-    expect(outputCreateProject.id).toBeDefined();
-    const projectId = outputCreateProject.id;
+  //When
+  const responseDelete = await axios.delete(
+    `http://localhost:3000/projects/${projectId}`
+  );
+  //Then
+  expect(responseDelete.status).toBe(204);
 
-    //When
-    const responseDelete = await axios.delete(`http://localhost:3000/projects/${projectId}`);
+  //Verifica se o projeto foi realmente deletado
+  try {
+    await axios.get(`http://localhost:3000/projects/${projectId}`);
+  } catch (error: any) {
+    expect(error.response.status).toBe(404);
+    expect(error.response.data.message).toBe("Projeto não encontrado");
+  }
+});
+
+test("Não deve criar um projeto se o nome for vazio", async () => {
+  //Given
+  const input = {
+    name: "",
+  };
+  //When
+  try {
+    await axios.post("http://localhost:3000/projects", input);
+  } catch (error: any) {
     //Then
-    expect(responseDelete.status).toBe(204);
-    
-    //Verifica se o projeto foi realmente deletado
-    try {
-        await axios.get(`http://localhost:3000/projects/${projectId}`);
-    } catch (error: any) {
-        expect(error.response.status).toBe(404);
-        expect(error.response.data.message).toBe("Projeto não encontrado");
-    }
+    expect(error.response.status).toBe(400);
+    expect(error.response.data.message).toBe("Nome do projeto é obrigatório");
+  }
 });
