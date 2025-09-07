1dadd1459ee5e8fd4cbb435e06736d008959d9b2
Thiago Siqueira <thiagosiqueira@unifei.edu.br>
2025-09-07 20:40:18 -0300

    fix: melhora estilização

--- frontend/src/views/dashboard/Index.vue
+++ frontend/src/views/dashboard/Index.vue
@@ -4,9 +4,52 @@ import Sidebar from '../../components/sidebar/Sidebar.vue'
 </script>
 
 <template>
-  <div class="d-flex">
-    <ReportPill title="Tarefas pendentes" :value="3" color="grey" width="33%" class="me-4" />
-    <ReportPill title="Tarefas em andamento" :value="3" color="primary" width="33%" class="me-4" />
-    <ReportPill title="Tarefas concluídas" :value="3" color="green" width="33%" class="me-4" />
-  </div>
+  <v-container fluid class="pa-4">
+    <v-row >
+      <v-col cols="12" md="4">
+        <ReportPill
+          title="Tarefas pendentes"
+          :value="3"
+          color="grey"
+          rounded="false"
+        />
+      </v-col>
+
+      <v-col cols="12" md="4">
+        <ReportPill
+          title="Tarefas em andamento"
+          :value="3"
+          color="primary"
+          rounded="false"
+        />
+      </v-col>
+
+      <v-col cols="12" md="4">
+        <ReportPill
+          title="Tarefas concluídas"
+          :value="3"
+          color="green"
+          rounded="false"
+        />
+      </v-col>
+
+      <!-- 4º item vai automaticamente para a 2ª linha -->
+      <v-col cols="12" md="4">
+        <ReportPill
+          title="Tarefas atrasadas"
+          :value="3"
+          color="teal"
+          rounded="false"
+        />
+      </v-col>
+            <v-col cols="12" md="4">
+        <ReportPill
+          title="Tarefas no prazo"
+          :value="3"
+          color="teal"
+          rounded="false"
+        />
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
